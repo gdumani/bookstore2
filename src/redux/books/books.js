c7b5d303b35dbc981132e82439c4a34b2c908749
gdumani <gdumani@gmail.com>
2022-03-02 17:21:00 -0600

    add remove button action

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,30 +1,44 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
-const initialState = [];
+const initialState = [
+  {
+    id: 1,
+    title: 'The Hunger Game',
+    author: 'Suzanne Collins',
+    category: 'action',
+  },
+  {
+    id: 2,
+    title: 'Dune',
+    author: 'Frank Herbert',
+    category: 'science fiction',
+  },
+  {
+    id: 3,
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+    category: 'economy',
+  },
+];
 
 export const addBook = (payload) => ({
   type: ADD_BOOK,
   payload,
 });
-export const removeBook = (payload) => ({
+
+export const removeBook = (id) => ({
   type: REMOVE_BOOK,
-  payload,
+  id,
 });
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
-      /*
-          return a new state object in which the books array will contain a new book object, passed by action.payload.
-          Remember -  you MUSN'T mutate the state. You have to return a new state object - i.e.:
-          return [ ...state, action.payload];
-          */
+      return [...state, action.payload];
+
     case REMOVE_BOOK:
-      /*
-          use ES6 filter() method to create a new array, which will not contain the book you want to remove from the store (filter by the id key - i.e.:
-          return state.filter(book => book.id !== id);
-          */
+      return [...state].filter((book) => book.id !== action.id);
 
     default:
       return state;

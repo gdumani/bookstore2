88daadbd1079dbf1786ec826f0b2dfef1b754831
gdumani <gdumani@gmail.com>
2022-03-02 13:31:20 -0600

    create routes and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom'
 import './App.css';
+import NavBar from './components/navbar';
+import Categories from './pages/categories';
+import Books from './pages/books';
 
 function App() {
   return (
-
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </Router>
   );
 }
 
